1eaf19a19d910a1a477309a43f0aacf05ca3826f
peligro <yo@cesarcancino.com>
2019-04-08 12:08:34 -0400

    h

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::get('/', 'IndexController@index')->name('index_index');
-Route::post('api/v1/alimentador', 'RestController@alimentador')->name('alimentador');
+Route::get('api/v1/alimentador', 'RestController@alimentador')->name('alimentador');
